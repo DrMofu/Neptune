af7cf92139b922033493c700e42d6c77c54aa21b
MrMofu <mr_mofu@163.com>
2017-12-05 13:10:23 +0800

    12.5丰富界面

--- src/view/MBattleView.java
+++ src/view/MBattleView.java
@@ -1,5 +1,35 @@
 package view;
 
-public class MBattleView {
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MBattleView extends JFrame{
+	/** 信息窗口的宽度 */
+	private static final int INFO_W = 1280;
+	/** 信息窗口的高度 */
+	private static final int INFO_H = 680;
+	public MBattleView(String name){
+		super(name);
+		battleViewInit();
+		this.pack();
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	private void battleViewInit() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocation(40, 20);
+		this.setPreferredSize(new Dimension(INFO_W, INFO_H));
+		// 添加成员组件
+        JPanel contentPane=new JPanel();
+        this.setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.setBackground(new Color(220,220,220));
+		initBattleAera(contentPane);		
+	}
+	private void initBattleAera(JPanel contentPane) {		
+		
+	}
 }
